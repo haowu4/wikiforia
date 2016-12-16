ce5c5b48c445fba7b05bc9d1ca045b9d68f88352
Hao Wu <haowu@haowu.me>
2016-12-16 01:08:44 -0800

    update

--- src/main/java/se/lth/cs/nlp/wikipedia/parser/SwebleTextAstWalker.java
+++ src/main/java/se/lth/cs/nlp/wikipedia/parser/SwebleTextAstWalker.java
@@ -1,16 +1,16 @@
 /**
  * This file is part of Wikiforia.
- *
+ * <p>
  * Wikiforia is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * Wikiforia is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,6 +23,8 @@ import org.sweble.wikitext.engine.config.WikiConfig;
 import org.sweble.wikitext.engine.nodes.EngPage;
 import org.sweble.wikitext.parser.nodes.*;
 import org.sweble.wikitext.parser.parser.LinkTargetException;
+
+import java.net.URLEncoder;
 
 /**
  * A Sweble AST walker that extracts text content from the parser AST tree.
@@ -37,17 +39,20 @@ public class SwebleTextAstWalker extends AstVisitor<WtNode> {
     private boolean filterOutput;
     private boolean expectSectionTitle;
 
+    private boolean expectedTitleHref = false;
+
+    public static String encodeURL(String in) {
+        return in.replaceAll(" ", "_");
+    }
 
     // =========================================================================
 
-    public SwebleTextAstWalker(WikiConfig config)
-    {
+    public SwebleTextAstWalker(WikiConfig config) {
         this.config = config;
     }
 
     @Override
-    protected WtNode before(WtNode node)
-    {
+    protected WtNode before(WtNode node) {
         // This method is called by go() before visitation starts
         sb = new StringBuilder();
         currentSectionTitle = "";
@@ -56,166 +61,145 @@ public class SwebleTextAstWalker extends AstVisitor<WtNode> {
     }
 
     @Override
-    protected Object after(WtNode node, Object result)
-    {
+    protected Object after(WtNode node, Object result) {
         return sb.toString();
     }
 
     // =========================================================================
 
-    public void visit(WtNode n)
-    {
+    public void visit(WtNode n) {
 
     }
 
-    public void visit(WtNodeList n)
-    {
+    public void visit(WtNodeList n) {
         iterate(n);
     }
 
-    public void visit(WtUnorderedList e)
-    {
+    public void visit(WtUnorderedList e) {
         //int start = sb.length();
         iterate(e);
         //int end = sb.length();
         sb.append("\n\n");
     }
 
-    public void visit(WtOrderedList e)
-    {
+    public void visit(WtOrderedList e) {
         //int start = sb.length();
         iterate(e);
         //int end = sb.length();
         sb.append("\n\n");
     }
 
-    public void visit(WtListItem item)
-    {
+    public void visit(WtListItem item) {
         int start = sb.length();
         iterate(item);
         int end = sb.length();
 
-        if(start != end) {
-            String endpart = StringUtils.trim(sb.substring(start,end));
-            if(endpart.length() > 0 && !endpart.endsWith("."))
+        if (start != end) {
+            String endpart = StringUtils.trim(sb.substring(start, end));
+            if (endpart.length() > 0 && !endpart.endsWith("."))
                 sb.append(". ");
             else
                 sb.append(" ");
         }
     }
 
-    public void visit(EngPage p)
-    {
+    public void visit(EngPage p) {
         iterate(p);
     }
 
-    public void visit(WtText text)
-    {
-        if(expectSectionTitle) {
-            if(!text.getContent().trim().equalsIgnoreCase("")) {
+    public void visit(WtText text) {
+        if (expectedTitleHref) {
+            sb.append(encodeURL(text.getContent()));
+            return;
+        }
+
+        if (expectSectionTitle) {
+            if (!text.getContent().trim().equalsIgnoreCase("")) {
                 currentSectionTitle = text.getContent().trim();
             }
             expectSectionTitle = false;
         }
 
-        if(!isInsideFilteredSection() && !text.getContent().replaceAll("[\n\r]", "").equalsIgnoreCase("")) {
+        if (!isInsideFilteredSection() && !text.getContent().replaceAll("[\n\r]", "").equalsIgnoreCase("")) {
             sb.append(text.getContent());
         }
     }
 
-    public void visit(WtWhitespace w)
-    {
-        if(!isInsideFilteredSection()) {
+    public void visit(WtWhitespace w) {
+        if (!isInsideFilteredSection()) {
             sb.append(" ");
         }
     }
 
-    public void visit(WtBold b)
-    {
+    public void visit(WtBold b) {
         iterate(b);
     }
 
-    public void visit(WtItalics i)
-    {
+    public void visit(WtItalics i) {
         iterate(i);
     }
 
-    public void visit(WtXmlCharRef cr)
-    {
-        if(!isInsideFilteredSection()) {
+    public void visit(WtXmlCharRef cr) {
+        if (!isInsideFilteredSection()) {
             sb.append(Character.toChars(cr.getCodePoint()));
         }
     }
 
-    public void visit(WtXmlEntityRef er)
-    {
+    public void visit(WtXmlEntityRef er) {
         String ch = er.getResolved();
-        if (ch == null)
-        {
-            if(!isInsideFilteredSection()) {
+        if (ch == null) {
+            if (!isInsideFilteredSection()) {
                 sb.append('&' + er.getName() + ';');
             }
-        }
-        else
-        {
-            if(!isInsideFilteredSection()) {
+        } else {
+            if (!isInsideFilteredSection()) {
                 sb.append(ch);
             }
         }
 
     }
 
-    public void visit(WtUrl wtUrl)
-    {
-        if (!wtUrl.getProtocol().isEmpty())
-        {
-            if(!isInsideFilteredSection()) {
+    public void visit(WtUrl wtUrl) {
+        if (!wtUrl.getProtocol().isEmpty()) {
+            if (!isInsideFilteredSection()) {
                 sb.append(wtUrl.getProtocol() + ':');
             }
         }
-        if(!isInsideFilteredSection()) {
+        if (!isInsideFilteredSection()) {
             sb.append(wtUrl.getPath());
         }
     }
 
-    public void visit(WtExternalLink link)
-    {
+    public void visit(WtExternalLink link) {
     }
 
-    public void visit(WtInternalLink link)
-    {
-        try
-        {
-            if (link.getTarget().isResolved())
-            {
+    public void visit(WtInternalLink link) {
+        try {
+            if (link.getTarget().isResolved()) {
                 PageTitle page = PageTitle.make(config, link.getTarget().getAsString());
                 if (page.getNamespace().equals(config.getNamespace("Category"))) {
                     return;
-                }
-                else if(page.getNamespace().isFileNs() || page.getNamespace().isMediaNs()) {
+                } else if (page.getNamespace().isFileNs() || page.getNamespace().isMediaNs()) {
                     return;
                 }
             }
-        }
-        catch (LinkTargetException e)
-        {
+        } catch (LinkTargetException e) {
         }
 
         //int start = sb.length();
 
 
-
 //        String surface =
         // Target is the surface. Because there must be a surface text.
         sb.append("[");
-        if(!isInsideFilteredSection()) {
+        if (!isInsideFilteredSection()) {
             if (!link.getPrefix().isEmpty())
                 sb.append(link.getPrefix());
         }
 
-        if (link.hasTitle()){
+        if (link.hasTitle()) {
             iterate(link.getTitle());
-        }else{
+        } else {
             iterate(link.getTarget());
         }
 //        if (!link.hasTitle())
@@ -231,25 +215,25 @@ public class SwebleTextAstWalker extends AstVisitor<WtNode> {
 //            sb.append("~]");
 //        }
 
-        if(!isInsideFilteredSection()) {
+        if (!isInsideFilteredSection()) {
 //            if (!link.getPostfix().isEmpty())
-                sb.append(link.getPostfix());
+            sb.append(link.getPostfix());
         }
 
 
         sb.append("]");
         sb.append("(");
 
-
+        expectedTitleHref = true;
         iterate(link.getTarget());
+        expectedTitleHref = false;
         sb.append(")");
 
 //        sb.append("]");
         //int end = sb.length();
     }
 
-    public void visit(WtSection s)
-    {
+    public void visit(WtSection s) {
         //int start = sb.length();
 
         filterOutput = true;
@@ -262,9 +246,9 @@ public class SwebleTextAstWalker extends AstVisitor<WtNode> {
         expectSectionTitle = false;
         filterOutput = false;
 
-        if(!isInsideFilteredSection()) {
-            if(sb.length() > 0) {
-                if(sb.charAt(sb.length() -1) != '\n') {
+        if (!isInsideFilteredSection()) {
+            if (sb.length() > 0) {
+                if (sb.charAt(sb.length() - 1) != '\n') {
                     sb.append("\n\n");
                 }
             }
@@ -277,13 +261,12 @@ public class SwebleTextAstWalker extends AstVisitor<WtNode> {
         //for(int i = sb.length()-1; i > 0 && sb.charAt(i) == '\n'; i--, end--);
     }
 
-    public void visit(WtParagraph p)
-    {
+    public void visit(WtParagraph p) {
         //int start = sb.length();
         iterate(p);
-        if(!isInsideFilteredSection()) {
-            if(sb.length() > 0) {
-                if(sb.charAt(sb.length() -1) != '\n') {
+        if (!isInsideFilteredSection()) {
+            if (sb.length() > 0) {
+                if (sb.charAt(sb.length() - 1) != '\n') {
                     sb.append("\n\n");
                 }
             }
@@ -293,17 +276,12 @@ public class SwebleTextAstWalker extends AstVisitor<WtNode> {
         //for(int i = sb.length()-1; i > 0 && sb.charAt(i) == '\n'; i--, end--);
     }
 
-    public void visit(WtHorizontalRule hr)
-    {
+    public void visit(WtHorizontalRule hr) {
     }
 
-    public void visit(WtXmlElement e)
-    {
-        if (e.getName().equalsIgnoreCase("br") || e.getName().equalsIgnoreCase("gallery") || e.getName().equalsIgnoreCase("imagemap"))
-        {
-        }
-        else
-        {
+    public void visit(WtXmlElement e) {
+        if (e.getName().equalsIgnoreCase("br") || e.getName().equalsIgnoreCase("gallery") || e.getName().equalsIgnoreCase("imagemap")) {
+        } else {
             iterate(e.getBody());
         }
     }
@@ -311,51 +289,43 @@ public class SwebleTextAstWalker extends AstVisitor<WtNode> {
     // =========================================================================
     // Stuff we want to hide
 
-    public void visit(WtImageLink n)
-    {
+    public void visit(WtImageLink n) {
     }
 
-    public void visit(WtIllegalCodePoint n)
-    {
+    public void visit(WtIllegalCodePoint n) {
     }
 
-    public void visit(WtXmlComment n)
-    {
+    public void visit(WtXmlComment n) {
     }
 
-    public void visit(WtTemplate n)
-    {
-        if(!isInsideFilteredSection()) {
-            if(n.toString().equalsIgnoreCase("WtTemplate([0] = WtName[WtText(\"spaced ndash\")], [1] = WtTemplateArguments[])")) {
+    public void visit(WtTemplate n) {
+        if (!isInsideFilteredSection()) {
+            if (n.toString().equalsIgnoreCase("WtTemplate([0] = WtName[WtText(\"spaced ndash\")], [1] = WtTemplateArguments[])")) {
                 sb.append(" - ");
             }
         }
     }
 
-    public void visit(WtTemplateArgument n)
-    {
+    public void visit(WtTemplateArgument n) {
     }
 
-    public void visit(WtTemplateParameter n)
-    {
+    public void visit(WtTemplateParameter n) {
     }
 
-    public void visit(WtTagExtension n)
-    {
+    public void visit(WtTagExtension n) {
     }
 
-    public void visit(WtPageSwitch n)
-    {
+    public void visit(WtPageSwitch n) {
     }
 
     // =========================================================================
 
     private boolean isInsideFilteredSection() {
-        if(filterOutput) {
+        if (filterOutput) {
             return true;
         }
 
-        if(this.currentSectionTitle.equalsIgnoreCase("See also") ||
+        if (this.currentSectionTitle.equalsIgnoreCase("See also") ||
                 this.currentSectionTitle.equalsIgnoreCase("Notes") ||
                 this.currentSectionTitle.equalsIgnoreCase("Writings") ||
                 this.currentSectionTitle.equalsIgnoreCase("References") ||
